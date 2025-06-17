da633c55f0afb52705082ba94b3cf16e9642861f
jay patel <106934952+pateljay9936@users.noreply.github.com>
2025-06-17 12:48:01 -0400

    Create main.py - imports are done

---
+++ main.py
@@ -0,0 +1,12 @@
+import asyncio
+import os
+import re
+import trafilatura
+from litellm import completion
+from duckduckgo_search import AsyncDDGS
+from PyPDF2 import PdfReader
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler, filters, ApplicationBuilder
+from youtube_transcript_api import YouTubeTranscriptApi
