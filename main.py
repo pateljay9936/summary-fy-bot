20e78fdd44d1f824d598fb3d1b8a32d087040d0c
jay patel <106934952+pateljay9936@users.noreply.github.com>
2025-06-18 11:53:03 -0400

    Update main.py - added main secetion and ENV variables

--- main.py
+++ main.py
@@ -10,3 +10,31 @@ from tqdm import tqdm
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler, filters, ApplicationBuilder
 from youtube_transcript_api import YouTubeTranscriptApi
+
+telegram_token = os.environ.get("TELEGRAM_TOKEN", "xxx")
+model = os.environ.get("LLM_MODEL", "gpt-3.5-turbo-16k")
+lang = os.environ.get("TS_LANG", "Taiwanese Mandarin")
+ddg_region = os.environ.get("DDG_REGION", "wt-wt")
+chunk_size = int(os.environ.get("CHUNK_SIZE", 10000))
+allowed_users = os.environ.get("ALLOWED_USERS", "")
+
+
+def main():
+    try:
+        application = ApplicationBuilder().token(telegram_token).build()
+        start_handler = CommandHandler('start', handle_start)
+        help_handler = CommandHandler('help', handle_help)
+        summarize_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, handle_summarize)
+        file_handler = MessageHandler(filters.Document.PDF, handle_file)
+        button_click_handler = CallbackQueryHandler(handle_button_click)
+        application.add_handler(file_handler)
+        application.add_handler(start_handler)
+        application.add_handler(help_handler)
+        application.add_handler(summarize_handler)
+        application.add_handler(button_click_handler)
+        application.run_polling()
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
